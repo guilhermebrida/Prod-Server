df2963520c6e07e455bc80614e00a65e7d10214d
guilhermebrida <43304919+guilhermebrida@users.noreply.github.com>
2023-05-24 21:56:44 -0300

    correçao init db

--- db/init.sql
+++ db/init.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS copilotos.vozes
 (
     "IMEI" text COLLATE pg_catalog."default" NOT NULL,
     "SN" text COLLATE pg_catalog."default" NOT NULL,
-    "VOZES" bigint
+    "VOZES" bigint,
     reception_datetime timestamp with time zone DEFAULT timezone('America/Sao_Paulo', now())
     );
 INSERT INTO copilotos.vozes ("IMEI","SN","VOZES") VALUES ('teste','teste1',22);
