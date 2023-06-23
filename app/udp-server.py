f3ed1872000959697110d01b24a79852641665a2
guilhermebrida <43304919+guilhermebrida@users.noreply.github.com>
2023-06-23 19:10:11 -0300

    script basico

--- app/udp-server.py
+++ app/udp-server.py
@@ -145,7 +145,7 @@ class udp():
     async def criar(self,device_id):
         try:
             sn2 = RSN_DICT[device_id]
-            if self.fdir == 0:
+            if self.vozes == 0:
                 cursor.execute('INSERT INTO vozes ("IMEI", "SN", "VOZES") values (\'{}\', \'{}\', \'1\');'.format(device_id, sn2))
             else:
                 cursor.execute('INSERT INTO vozes ("IMEI", "SN", "VOZES") values (\'{}\', \'{}\', \'{}\');'.format(device_id, sn2,self.vozes))
