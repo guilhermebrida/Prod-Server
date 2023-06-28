aa2e48b936ad3206e86b01ccfbaabefb51280bb0
guilhermebrida <43304919+guilhermebrida@users.noreply.github.com>
2023-06-28 00:31:54 -0300

    teste server

--- app/FOTA.py
+++ app/FOTA.py
@@ -108,7 +108,7 @@ async def envioScript(sock, device_id, addr):
                     xvm = XVM.generateXVM(device_id, str(8010+i).zfill(4), comandos[i])
                     sock.sendto(xvm.encode(), addr)
                     await asyncio.wait_for(receber_resposta(sock), timeout=3)
-                    await asyncio.sleep(0.1)
+                    # await asyncio.sleep(0.1)
                     break
                 except asyncio.TimeoutError:
                     tentativas += 1
@@ -120,7 +120,7 @@ async def envioScript(sock, device_id, addr):
 
 async def receber_resposta(sock):
     response, _ = sock.recvfrom(1024)
-    print('Resposta do equipamento:', response.decode())
+    print('Resposta do equipamento:', response)
 
 
 # async def fdir(sock, device_id, addr):
