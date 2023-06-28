83eadf83110ef969af5cdf8e1575dacf2b71dfb3
guilhermebrida <43304919+guilhermebrida@users.noreply.github.com>
2023-06-27 23:53:25 -0300

    teste server

--- app/XVM.py
+++ app/XVM.py
@@ -19,9 +19,7 @@ def generateAck(id,sequence):
     return resp
 
 def generateXVM(id,sequence,message):
-    print(type(id),type(sequence),type(message))
     resp = message+';ID='+id+';#'+sequence+';*'
-    print(resp)
     resp = resp+format(calcCheckSum(resp),'02X')+'<\r\n'
     return resp
 
